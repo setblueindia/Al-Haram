f5672432071427eb4ee5145751383ab85e985e62
Niraj Mehta <niraj.mehta@emvigotech.com>
2024-07-27 13:09:46 +0530

    Push notification / android

--- App.js
+++ App.js
@@ -3,55 +3,32 @@ import React, { useEffect, useState } from 'react';
 import AppNavigation from './src/navigation/AppNavigation';
 import NetworkConnection from './src/components/NetworkConnection';
 import NetInfo from '@react-native-community/netinfo';
+import messaging from '@react-native-firebase/messaging';
 
-// import CusLoader from './src/components/CustomLoader';
-// import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
   const [isConnected, setIsConnected] = useState(true);
-  // const loader = useSelector(state => state?.Categories?.loader);
+  
+  useEffect(() => {
+    const checkPermission = async () => {
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        console.log('Authorization status:', authStatus);
+        const token = await messaging().getToken();
+        console.log('FCM Token:', token);
+      } else {
+        Alert.alert('Permission Denied', 'You need to grant notification permissions to receive notifications.');
+      }
+    };
+    checkPermission();
+  }, []);
 
 
-  // const requestUserPermission = async () => {
-  //   const authStatus = await messaging().requestPermission();
-  //   const enabled =
-  //     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-  //     authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-  //   if (enabled) {
-  //     console.log('Authorization status:', authStatus);
-  //   }
-  // };
-
-  // const getToken = async () => {
-  //   const token = await messaging().getToken();
-  //   console.log('Token = ', token);
-  // };
-
-  // useEffect(() => {
-  //   const setupMessaging = async () => {
-  //     await requestUserPermission();
-  //     await getToken();
-
-  //     const unsubscribe = messaging().onMessage(async remoteMessage => {
-  //       Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-  //     });
-
-  //     return unsubscribe;
-  //   };
-
-  //   const messagingSetup = setupMessaging();
-
-  //   return () => {
-  //     messagingSetup.then(unsubscribe => {
-  //       if (typeof unsubscribe === 'function') {
-  //         unsubscribe();
-  //       } else {
-  //         console.log('Unsubscribe is not a function:', unsubscribe);
-  //       }
-  //     });
-  //   };
-  // }, []);
 
   useEffect(() => {
     const unsubscribe = NetInfo.addEventListener(state => {
